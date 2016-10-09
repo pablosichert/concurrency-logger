612908bf20ae88903ed68488091eb6654688f491
Pablo Sichert <mail@pablosichert.de>
2016-10-09 20:53:54 +0200

    Make options.getLevel: responseTime => alertLevel configurable

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 import colors from 'ansi-256-colors';
 
 const SPACER = colors.fg.getRgb(1, 1, 1) + '┈' + colors.reset;
+const GET_LEVEL = responseTime => Math.floor(responseTime / 50) - 1;
 
-export default function createLogger(options) {
+export default function createLogger(options = {}) {
     const {
-        minSlots = 3
+        minSlots = 3,
+        getLevel = GET_LEVEL
     } = options;
 
     const slots = new Array(minSlots).fill(null);
+    const colorize = responseTime => character => {
+        const level = getLevel(responseTime);
 
-    const getShade = function (start, now) {
-        const time = now - start;
-        const alertLevel = Math.floor((time - 50) / 50);
-
-        if (alertLevel <= 0) {
-            return '';
+        if (level <= 0) {
+            return character;
         }
 
-        return colors.fg.getRgb(5, Math.max(6 - alertLevel, 0), 0);
+        let green = 6 - level;
+
+        if (green < 0) {
+            green = 0;
+        }
+
+        return colors.fg.getRgb(5, green, 0) + character + colors.reset;
     };
 
     return async function logger(context, next) {
@@ -36,7 +42,7 @@ export default function createLogger(options) {
             slot = slots.length - 1;
         }
 
-        const openSlot = slots.map(slot => slot ? getShade(slot, start) + '│' + colors.reset : SPACER);
+        const openSlot = slots.map(slot => slot ? colorize(start - slot)('│') : SPACER);
         openSlot[slot] = '┬';
         slots[slot] = start;
 
@@ -53,8 +59,8 @@ export default function createLogger(options) {
             time = new Array(6 - time.length).join(SPACER) + time;
         }
 
-        const closeSlot = slots.map(slot => slot ? getShade(slot, end) + '│' + colors.reset : SPACER);
-        closeSlot[slot] = getShade(slots[slot], end) + '┴' + colors.reset;
+        const closeSlot = slots.map(slot => slot ? colorize(end - slot)('│') : SPACER);
+        closeSlot[slot] = colorize(end - slots[slot])('┴');
         slots[slot] = null;
 
         // eslint-disable-next-line no-console
