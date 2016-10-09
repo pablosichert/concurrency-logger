868c463a2ba19be5d2b0afdabb970552ccb809d6
Pablo Sichert <mail@pablosichert.de>
2016-10-09 20:06:37 +0200

    Restructure constants

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 import colors from 'ansi-256-colors';
 
-const spacer = colors.fg.getRgb(1, 1, 1) + '┈' + colors.reset;
-const getShade = function (start, now) {
-    const time = now - start;
-    const alertLevel = Math.floor((time - 50) / 50);
-
-    if (alertLevel <= 0) {
-        return '';
-    }
-
-    return colors.fg.getRgb(5, Math.max(6 - alertLevel, 0), 0);
-};
+const SPACER = colors.fg.getRgb(1, 1, 1) + '┈' + colors.reset;
 
 export default function createLogger() {
     const slots = new Array(3).fill(null);
+
+    const getShade = function (start, now) {
+        const time = now - start;
+        const alertLevel = Math.floor((time - 50) / 50);
+
+        if (alertLevel <= 0) {
+            return '';
+        }
+
+        return colors.fg.getRgb(5, Math.max(6 - alertLevel, 0), 0);
+    };
 
     return async function logger(context, next) {
         const start = Date.now();
@@ -31,12 +32,12 @@ export default function createLogger() {
             slot = slots.length - 1;
         }
 
-        const openSlot = slots.map(slot => slot ? getShade(slot, start) + '│' + colors.reset : spacer);
+        const openSlot = slots.map(slot => slot ? getShade(slot, start) + '│' + colors.reset : SPACER);
         openSlot[slot] = '┬';
         slots[slot] = start;
 
         // eslint-disable-next-line no-console
-        console.log(`${new Array(4).join(spacer)} ⇾ ${context.method} ${new Array(6).join(spacer)} ${openSlot.join(spacer)} ${context.originalUrl}`);
+        console.log(`${new Array(4).join(SPACER)} ⇾ ${context.method} ${new Array(6).join(SPACER)} ${openSlot.join(SPACER)} ${context.originalUrl}`);
 
         await next();
 
@@ -45,14 +46,14 @@ export default function createLogger() {
         let time = `${end - start}ms`;
 
         if (time.length < 5) {
-            time = new Array(6 - time.length).join(spacer) + time;
+            time = new Array(6 - time.length).join(SPACER) + time;
         }
 
-        const closeSlot = slots.map(slot => slot ? getShade(slot, end) + '│' + colors.reset : spacer);
+        const closeSlot = slots.map(slot => slot ? getShade(slot, end) + '│' + colors.reset : SPACER);
         closeSlot[slot] = getShade(slots[slot], end) + '┴' + colors.reset;
         slots[slot] = null;
 
         // eslint-disable-next-line no-console
-        console.log(`${context.status} ⇽ ${context.method} ${time} ${closeSlot.join(spacer)} ${context.originalUrl}`);
+        console.log(`${context.status} ⇽ ${context.method} ${time} ${closeSlot.join(SPACER)} ${context.originalUrl}`);
     };
 }
