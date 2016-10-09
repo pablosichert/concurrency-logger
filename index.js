31d8f1fd03e1b707e78c36b4db063454e716f9f3
Pablo Sichert <mail@pablosichert.de>
2016-10-09 22:21:47 +0200

    Extract colorize function for reuse

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ import colors from 'ansi-256-colors';
 const SPACER = colors.fg.getRgb(1, 1, 1) + '┈' + colors.reset;
 const GET_LEVEL = responseTime => Math.floor(responseTime / 50) - 1;
 
+function colorize(level = 0, character) {
+    if (level <= 0) {
+        return character;
+    }
+
+    let green = 6 - level;
+
+    if (green < 0) {
+        green = 0;
+    }
+
+    return colors.fg.getRgb(5, green, 0) + character + colors.reset;
+}
+
 export default function createLogger(options = {}) {
     const {
         minSlots = 3,
@@ -10,20 +24,10 @@ export default function createLogger(options = {}) {
     } = options;
 
     const slots = new Array(minSlots).fill(null);
-    const colorizer = responseTime => character => {
+    const colorizer = responseTime => {
         const level = getLevel(responseTime);
 
-        if (level <= 0) {
-            return character;
-        }
-
-        let green = 6 - level;
-
-        if (green < 0) {
-            green = 0;
-        }
-
-        return colors.fg.getRgb(5, green, 0) + character + colors.reset;
+        return colorize.bind(null, level);
     };
 
     return async function logger(context, next) {
@@ -60,7 +64,7 @@ export default function createLogger(options = {}) {
 
         const end = Date.now();
         const responseTime = end - start;
-        const colorize = colorizer(responseTime);
+        const timeColorize = colorizer(responseTime);
 
         let time = `${responseTime}ms`;
 
@@ -68,10 +72,10 @@ export default function createLogger(options = {}) {
             time = new Array(6 - time.length).join(SPACER) + time;
         }
 
-        time = colorize(time);
+        time = timeColorize(time);
 
         const closeSlot = slots.map(slot => slot ? colorizer(end - slot)('│') : SPACER);
-        closeSlot[slot] = colorize('┴');
+        closeSlot[slot] = timeColorize('┴');
         slots[slot] = null;
 
         // eslint-disable-next-line no-console
