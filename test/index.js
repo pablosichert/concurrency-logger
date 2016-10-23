5be19970768a485c1ac1624f92f17e89e34a6d63
Pablo Sichert <mail@pablosichert.de>
2016-10-23 02:46:02 +0200

    Remove obsolete "describe" block

--- test/index.js
+++ test/index.js
@@ -132,263 +132,261 @@ describe('logger', () => {
         this.clock.restore();
     });
 
-    describe('configured with defaults', () => {
-        it('should log a request and a response', async function () {
-            const title = this.test.fullTitle();
-            const createLogger = this.createLogger(title);
+    it('should log a request and a response', async function () {
+        const title = this.test.fullTitle();
+        const createLogger = this.createLogger(title);
 
-            const logger = createLogger();
+        const logger = createLogger();
 
+        const context = {
+            method: 'GET',
+            originalUrl: '/'
+        };
+
+        const next = () => {
+            context.status = 200;
+        };
+
+        await logger(context, next);
+
+        expect(this.output, 'to equal', fixtures[title]);
+    });
+
+    describe('status codes', () => {
+        const types = {
+            '1xx (Informational)': [
+                100, 101, 102
+            ],
+            '2xx (Success)': [
+                200, 201, 202, 203, 204, 205, 206, 207, 208, 226,
+            ],
+            '3xx (Redirection)': [
+                300, 301, 302, 303, 304, 305, 306, 307, 308,
+            ],
+            '4xx (Client error)': [
+                400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 420, 421, 422, 423, 424, 425, 426, 428, 429, 431, 451, 444, 449,
+            ],
+            '5xx (Server error)': [
+                500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511
+            ]
+        };
+
+        for (const type of Object.keys(types)) {
+            it(`should format ${type}`, async function () {
+                const title = this.test.fullTitle();
+                const createLogger = this.createLogger(title);
+
+                const logger = createLogger();
+
+                for (const status of types[type]) {
+                    const context = {
+                        method: 'GET',
+                        originalUrl: '/'
+                    };
+
+                    const next = () => {
+                        context.status = status;
+                    };
+
+                    await logger(context, next);
+                }
+
+                expect(this.output, 'to equal', fixtures[title]);
+            });
+        }
+    });
+
+    it('should format response time', async function () {
+        const title = this.test.fullTitle();
+        const createLogger = this.createLogger(title);
+
+        const logger = createLogger();
+
+        for (const responseTime of [
+            0, 100, 1000, 10000, 100000, 1000000
+        ]) {
             const context = {
                 method: 'GET',
                 originalUrl: '/'
             };
 
             const next = () => {
+                this.clock.tick(responseTime);
+
                 context.status = 200;
             };
 
             await logger(context, next);
+        }
 
-            expect(this.output, 'to equal', fixtures[title]);
-        });
+        expect(this.output, 'to equal', fixtures[title]);
+    });
 
-        describe('status codes', () => {
-            const types = {
-                '1xx (Informational)': [
-                    100, 101, 102
-                ],
-                '2xx (Success)': [
-                    200, 201, 202, 203, 204, 205, 206, 207, 208, 226,
-                ],
-                '3xx (Redirection)': [
-                    300, 301, 302, 303, 304, 305, 306, 307, 308,
-                ],
-                '4xx (Client error)': [
-                    400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 420, 421, 422, 423, 424, 425, 426, 428, 429, 431, 451, 444, 449,
-                ],
-                '5xx (Server error)': [
-                    500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511
-                ]
+    it('should color response time', async function () {
+        const title = this.test.fullTitle();
+        const createLogger = this.createLogger(title);
+
+        const logger = createLogger();
+
+        const resolvers = [];
+        const loggers = [];
+
+        for (let i = 0; i < 8; i++) {
+            const context = {
+                method: 'GET',
+                originalUrl: '/'
             };
 
-            for (const type of Object.keys(types)) {
-                it(`should format ${type}`, async function () {
-                    const title = this.test.fullTitle();
-                    const createLogger = this.createLogger(title);
-
-                    const logger = createLogger();
-
-                    for (const status of types[type]) {
-                        const context = {
-                            method: 'GET',
-                            originalUrl: '/'
-                        };
-
-                        const next = () => {
-                            context.status = status;
-                        };
-
-                        await logger(context, next);
-                    }
-
-                    expect(this.output, 'to equal', fixtures[title]);
+            const next = async () => {
+                await new Promise(resolve => {
+                    resolvers.push(resolve);
                 });
-            }
-        });
-
-        it('should format response time', async function () {
-            const title = this.test.fullTitle();
-            const createLogger = this.createLogger(title);
-
-            const logger = createLogger();
-
-            for (const responseTime of [
-                0, 100, 1000, 10000, 100000, 1000000
-            ]) {
-                const context = {
-                    method: 'GET',
-                    originalUrl: '/'
-                };
-
-                const next = () => {
-                    this.clock.tick(responseTime);
-
-                    context.status = 200;
-                };
-
-                await logger(context, next);
-            }
-
-            expect(this.output, 'to equal', fixtures[title]);
-        });
-
-        it('should color response time', async function () {
-            const title = this.test.fullTitle();
-            const createLogger = this.createLogger(title);
-
-            const logger = createLogger();
-
-            const resolvers = [];
-            const loggers = [];
-
-            for (let i = 0; i < 8; i++) {
-                const context = {
-                    method: 'GET',
-                    originalUrl: '/'
-                };
-
-                const next = async () => {
-                    await new Promise(resolve => {
-                        resolvers.push(resolve);
-                    });
-
-                    context.status = 200;
-                };
-
-                loggers.push(logger(context, next));
-            }
-
-            while (resolvers.length) {
-                resolvers.pop()();
-                await loggers.pop();
-                this.clock.tick(50);
-            }
-
-            expect(this.output, 'to equal', fixtures[title]);
-        });
-
-        it('should log an unhandled error and set status to 500', async function () {
-            const title = this.test.fullTitle();
-            const createLogger = this.createLogger(title);
-
-            const logger = createLogger();
-
-            const context = {
-                method: 'GET',
-                originalUrl: '/'
-            };
-
-            const next = () => {
-                const error = new Error();
-                error.stack = 'Error\n    at stack';
-
-                throw error;
-            };
-
-            await logger(context, next);
-
-            expect(this.output, 'to equal', fixtures[title]);
-        });
-
-        it('should expose context.log method', async function() {
-            const title = this.test.fullTitle();
-            const createLogger = this.createLogger(title);
-
-            const logger = createLogger();
-
-            const context = {
-                method: 'GET',
-                originalUrl: '/'
-            };
-
-            const next = () => {
-                context.log('Log!');
 
                 context.status = 200;
             };
 
-            await logger(context, next);
+            loggers.push(logger(context, next));
+        }
 
-            expect(this.output, 'to equal', fixtures[title]);
-        });
+        while (resolvers.length) {
+            resolvers.pop()();
+            await loggers.pop();
+            this.clock.tick(50);
+        }
 
-        it('should expose context.log.info method', async function() {
-            const title = this.test.fullTitle();
-            const createLogger = this.createLogger(title);
+        expect(this.output, 'to equal', fixtures[title]);
+    });
 
-            const logger = createLogger();
+    it('should log an unhandled error and set status to 500', async function () {
+        const title = this.test.fullTitle();
+        const createLogger = this.createLogger(title);
 
-            const context = {
-                method: 'GET',
-                originalUrl: '/'
-            };
+        const logger = createLogger();
 
-            const next = () => {
-                context.log.info('Info!');
+        const context = {
+            method: 'GET',
+            originalUrl: '/'
+        };
 
-                context.status = 200;
-            };
+        const next = () => {
+            const error = new Error();
+            error.stack = 'Error\n    at stack';
 
-            await logger(context, next);
+            throw error;
+        };
 
-            expect(this.output, 'to equal', fixtures[title]);
-        });
+        await logger(context, next);
 
-        it('should expose context.log.error method', async function() {
-            const title = this.test.fullTitle();
-            const createLogger = this.createLogger(title);
+        expect(this.output, 'to equal', fixtures[title]);
+    });
 
-            const logger = createLogger();
+    it('should expose context.log method', async function() {
+        const title = this.test.fullTitle();
+        const createLogger = this.createLogger(title);
 
-            const context = {
-                method: 'GET',
-                originalUrl: '/'
-            };
+        const logger = createLogger();
 
-            const next = () => {
-                context.log.error('Error!');
+        const context = {
+            method: 'GET',
+            originalUrl: '/'
+        };
 
-                context.status = 200;
-            };
+        const next = () => {
+            context.log('Log!');
 
-            await logger(context, next);
+            context.status = 200;
+        };
 
-            expect(this.output, 'to equal', fixtures[title]);
-        });
+        await logger(context, next);
 
-        it('should pretty print functions', async function() {
-            const title = this.test.fullTitle();
-            const createLogger = this.createLogger(title);
+        expect(this.output, 'to equal', fixtures[title]);
+    });
 
-            const logger = createLogger();
+    it('should expose context.log.info method', async function() {
+        const title = this.test.fullTitle();
+        const createLogger = this.createLogger(title);
 
-            const context = {
-                method: 'GET',
-                originalUrl: '/'
-            };
+        const logger = createLogger();
 
-            const next = () => {
-                context.log(Function);
+        const context = {
+            method: 'GET',
+            originalUrl: '/'
+        };
 
-                context.status = 200;
-            };
+        const next = () => {
+            context.log.info('Info!');
 
-            await logger(context, next);
+            context.status = 200;
+        };
 
-            expect(this.output, 'to equal', fixtures[title]);
-        });
+        await logger(context, next);
 
-        it('should pretty print objects', async function() {
-            const title = this.test.fullTitle();
-            const createLogger = this.createLogger(title);
+        expect(this.output, 'to equal', fixtures[title]);
+    });
 
-            const logger = createLogger();
+    it('should expose context.log.error method', async function() {
+        const title = this.test.fullTitle();
+        const createLogger = this.createLogger(title);
 
-            const context = {
-                method: 'GET',
-                originalUrl: '/'
-            };
+        const logger = createLogger();
 
-            const next = () => {
-                context.log({ foo: 123 });
+        const context = {
+            method: 'GET',
+            originalUrl: '/'
+        };
 
-                context.status = 200;
-            };
+        const next = () => {
+            context.log.error('Error!');
 
-            await logger(context, next);
+            context.status = 200;
+        };
 
-            expect(this.output, 'to equal', fixtures[title]);
-        });
+        await logger(context, next);
+
+        expect(this.output, 'to equal', fixtures[title]);
+    });
+
+    it('should pretty print functions', async function() {
+        const title = this.test.fullTitle();
+        const createLogger = this.createLogger(title);
+
+        const logger = createLogger();
+
+        const context = {
+            method: 'GET',
+            originalUrl: '/'
+        };
+
+        const next = () => {
+            context.log(Function);
+
+            context.status = 200;
+        };
+
+        await logger(context, next);
+
+        expect(this.output, 'to equal', fixtures[title]);
+    });
+
+    it('should pretty print objects', async function() {
+        const title = this.test.fullTitle();
+        const createLogger = this.createLogger(title);
+
+        const logger = createLogger();
+
+        const context = {
+            method: 'GET',
+            originalUrl: '/'
+        };
+
+        const next = () => {
+            context.log({ foo: 123 });
+
+            context.status = 200;
+        };
+
+        await logger(context, next);
+
+        expect(this.output, 'to equal', fixtures[title]);
     });
 });
